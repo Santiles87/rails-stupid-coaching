0848c77c59f4d7fce16557e6360f6768192b6c9a
Fernando Silva <santiles87@gmail.com>
2022-07-07 20:43:59 -0300

    challenge solved

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,23 @@
 class QuestionsController < ApplicationController
   def ask
+
   end
 
   def answer
+    @question = params[:question]
+    @answer = coach_answer
   end
 
   private
-  def coach_answer(question)
-    if question.dowcase == 'I am going to work'
-      'Great'
-    elsif question.end_with?("?")
-      "Silly question, get dressed and go to work!"
+
+  def coach_answer
+    if @question.downcase == "I am going to work right now"
+      ''
+    elsif @question.end_with?('?')
+      "Silly question, get dressed and go to work"
+
     else
-      "I dont't care, get dressed and go to work!"
+      "I dont care, get dressed and go to work!"
     end
   end
 end
